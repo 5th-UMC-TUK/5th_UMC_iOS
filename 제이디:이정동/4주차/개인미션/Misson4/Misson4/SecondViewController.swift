9006c956b2ff71ef404fdc7aa4b7e6ff26bda6eb
Jeong Dong <eejxdong99@gmail.com>
2023-10-05 19:47:32 +0900

    4주차 미션

---
+++ 제이디:이정동/4주차/개인미션/Misson4/Misson4/SecondViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SecondViewController.swift
+//  Misson4
+//
+//  Created by 이정동 on 10/5/23.
+//
+
+import UIKit
+import SnapKit
+
+class SecondViewController: UIViewController {
+    
+    private lazy var backButton: UIButton = {
+        let view = UIButton()
+        view.setTitle("돌아가기", for: .normal)
+        return view
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+        
+        view.addSubview(backButton)
+        
+        backButton.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+        
+        backButton.addTarget(self, action: #selector(backButtonTapped), for: .touchUpInside)
+    }
+
+    @objc func backButtonTapped() {
+        UserDefaults.standard.removeObject(forKey: "@")
+        
+        (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?.changeRootViewController(ViewController())
+    }
+}
